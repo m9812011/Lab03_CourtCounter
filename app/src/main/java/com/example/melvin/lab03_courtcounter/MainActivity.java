c88b9930d03753150bedb3330f7473a54ab58872
m9812011 <m9812011@gmail.com>
2016-03-16 14:58:36 +0800

    添加換頁顯示命中率功能(Intent 傳值)

--- app/src/main/java/com/example/melvin/lab03_courtcounter/MainActivity.java
+++ app/src/main/java/com/example/melvin/lab03_courtcounter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.melvin.lab03_courtcounter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -10,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView scoreViewA,scoreViewB ;
     private int scoreTeamA = 0;
     private int scoreTeamB = 0;
-
+    private int m3pointForA,m2pointForA,m1pointForA = 0;
+    private int m3pointForB,m2pointForB,m1pointForB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +32,15 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()){
             case R.id.add3ForTeamA:
                 scoreTeamA += 3;
+                ++m3pointForA;
                 break;
             case R.id.add2ForTeamA:
                 scoreTeamA += 2;
+                ++m2pointForA;
                 break;
             case R.id.add1ForTeamA:
                 scoreTeamA += 1;
+                ++m1pointForA;
                 break;
             }
         scoreViewA.setText(String.valueOf(scoreTeamA));
@@ -47,12 +52,15 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()){
             case R.id.add3ForTeamB:
                 scoreTeamB += 3;
+                ++m3pointForB;
                 break;
             case R.id.add2ForTeamB:
                 scoreTeamB += 2;
+                ++m2pointForB;
                 break;
             case R.id.add1ForTeamB:
                 scoreTeamB += 1;
+                ++m1pointForB;
                 break;
         }
         scoreViewB.setText(String.valueOf(scoreTeamB));
@@ -63,11 +71,31 @@ public class MainActivity extends AppCompatActivity {
         if(view.getId() == R.id.reset){
             scoreTeamA = 0;
             scoreTeamB = 0;
+            m3pointForA =0;
+            m2pointForA =0;
+            m1pointForA =0;
+            m3pointForB =0;
+            m2pointForB =0;
+            m1pointForB =0;
             scoreViewA.setText(String.valueOf(scoreTeamA));
             scoreViewB.setText(String.valueOf(scoreTeamB));
         }
     }
 
+    public void dataAnalysis(View view){
+        Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+        Bundle bundle = new Bundle();
+        bundle.putInt("m3pointForA",m3pointForA);
+        bundle.putInt("m2pointForA",m2pointForA);
+        bundle.putInt("m1pointForA",m1pointForA);
+        bundle.putInt("m3pointForB",m3pointForB);
+        bundle.putInt("m2pointForB",m2pointForB);
+        bundle.putInt("m1pointForB",m1pointForB);
+        bundle.putInt("scoreTeamA",scoreTeamA);
+        bundle.putInt("scoreTeamB",scoreTeamB);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
 
 
 }
---
+++ app/src/main/java/com/example/melvin/lab03_courtcounter/MainActivity2.java
@@ -0,0 +1,75 @@
+package com.example.melvin.lab03_courtcounter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView threePointForA,twoPointForA,onePointForA;
+    private TextView threePointForB,twoPointForB,onePointForB;
+    private TextView scoreA,scoreB;
+    private LinearLayout lauout1,lauout2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        findViews();
+        getDataAndShowDate();
+    }
+
+
+    private void findViews(){
+        threePointForA = (TextView)findViewById(R.id.add3ForTeamA_text);
+        twoPointForA = (TextView)findViewById(R.id.add2ForTeamA_text);
+        onePointForA = (TextView)findViewById(R.id.add1ForTeamA_text);
+        threePointForB = (TextView)findViewById(R.id.add3ForTeamB_text);
+        twoPointForB = (TextView)findViewById(R.id.add2ForTeamB_text);
+        onePointForB = (TextView)findViewById(R.id.add1ForTeamB_text);
+        scoreA = (TextView)findViewById(R.id.scoreA);
+        scoreB = (TextView)findViewById(R.id.scoreB);
+        lauout1 = (LinearLayout)findViewById(R.id.titleA);
+        lauout2 = (LinearLayout)findViewById(R.id.titleB);
+    }
+
+
+    private void getDataAndShowDate() {
+        Bundle bundle = getIntent().getExtras();
+        int m3pointForA = bundle.getInt("m3pointForA");
+        int m2pointForA = bundle.getInt("m2pointForA");
+        int m1pointForA = bundle.getInt("m1pointForA");
+        int m3pointForB = bundle.getInt("m3pointForB");
+        int m2pointForB = bundle.getInt("m2pointForB");
+        int m1pointForB = bundle.getInt("m1pointForB");
+        int scoreTeamA = bundle.getInt("scoreTeamA");
+        int scoreTeamB = bundle.getInt("scoreTeamB");
+        threePointForA.setText("三分球 : " + String.valueOf(m3pointForA));
+        twoPointForA.setText("二分球 : " + String.valueOf(m2pointForA));
+        onePointForA.setText("罰球 : " + String.valueOf(m1pointForA));
+        threePointForB.setText("三分球 : " + String.valueOf(m3pointForB));
+        twoPointForB.setText("二分球 : " + String.valueOf(m2pointForB));
+        onePointForB.setText("罰球 : " + String.valueOf(m1pointForB));
+        scoreA.setText("總分 : " + String.valueOf(scoreTeamA));
+        scoreB.setText("總分 : " + String.valueOf(scoreTeamB));
+
+        if(scoreTeamA > scoreTeamB){
+            lauout1.setBackgroundResource(android.R.color.holo_orange_light);
+        }else{
+            lauout2.setBackgroundResource(android.R.color.holo_orange_light);
+        }
+
+    }
+
+
+    public void returnScore(View view) {
+        Intent intent = new Intent(MainActivity2.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
